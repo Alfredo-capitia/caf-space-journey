0cf5d57c37d530d8c82a062eb214ce640ba3b65e
Alfredo-capitia <delciiocapitia676@gmail.com>
2024-10-09 16:02:14 +0100

    remove same dependencs

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {App} from './app.tsx'
-import {  } from "./index.css";
-
+import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
